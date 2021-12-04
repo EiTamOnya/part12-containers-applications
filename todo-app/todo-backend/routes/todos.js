8f3b4a14e58165f63f562f559d2be4be54285871
EiTamOnya <pashov.pavel@gmail.com>
2021-12-04 13:04:36 +0200

    Exercise 12.9: Setup Redis to project Exercise 12.10

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,50 +1,59 @@
 const express = require('express');
+const redis = require('../redis')
 const { Todo } = require('../mongo')
 const router = express.Router();
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
-	const todos = await Todo.find({})
-	res.send(todos);
+  const todos = await Todo.find({})
+  res.send(todos);
 });
 
 /* POST todo to listing. */
 router.post('/', async (req, res) => {
-	const todo = await Todo.create({
-		text: req.body.text,
-		done: false
-	})
-	res.send(todo);
+  const todo = await Todo.create({
+    text: req.body.text,
+    done: false
+  })
+  const todoCount = await redis.getAsync('added_todos');
+  if (!todoCount) {
+    await redis.setAsync('added_todos', 1)
+  } else {
+    await redis.setAsync(
+      'added_todos', Number(todoCount) + 1
+    )
+  }
+  res.send(todo);
 });
 
 const singleRouter = express.Router();
 
 const findByIdMiddleware = async (req, res, next) => {
-	const { id } = req.params
-	req.todo = await Todo.findById(id)
-	if (!req.todo) return res.sendStatus(404)
-	next()
+  const { id } = req.params
+  req.todo = await Todo.findById(id)
+  if (!req.todo) return res.sendStatus(404)
+  next()
 
 }
 
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-	await req.todo.delete()
-	res.send(todo);
+  await req.todo.delete()
+  res.send(todo);
 });
 
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-	res.send(req.todo)
+  res.send(req.todo)
 });
 
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-	const updatedTodo = await Todo.findOneAndUpdate({ _id: req.todo.id }, { $set: { text: req.body.text, done: req.body.done } }, { new: true });
-	res.send(updatedTodo)
+  const updatedTodo = await Todo.findOneAndUpdate({ _id: req.todo.id }, { $set: { text: req.body.text, done: req.body.done } }, { new: true });
+  res.send(updatedTodo)
 });
 
 
